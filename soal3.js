a2b6c4ec21ab2be9038c6f939144ce1302ca0118
Alvan MAzid <alvantwent@gmail.com>
2023-09-07 15:46:07 +0700

    soal3

--- soal3.js
+++ soal3.js
@@ -1,6 +1,22 @@
-function getAngkaTerbesarKedua(personName) {}
+function getAngkaTerbesarKedua(personName) {
+  let angkaTerbesar = personName[0];
+  let angkaTerbesarKedua = personName[1];
 
-const dataAngka = [9, 4, 7, 7, 7, 3, 2, 2, 8];
+  for (let i = 2; i < personName.length; i++) {
+    if (personName[i] > angkaTerbesar) {
+      angkaTerbesarKedua = angkaTerbesar;
+      angkaTerbesar = personName[i];
+    } else if (
+      personName[i] > angkaTerbesarKedua &&
+      personName[i] !== angkaTerbesar
+    ) {
+      angkaTerbesarKedua = personName[i];
+    }
+  }
+  return angkaTerbesarKedua;
+}
+
+const dataAngka = [9, 4, 7, 3, 2, 8];
 console.log(getAngkaTerbesarKedua(dataAngka));
-console.log(getAngkaTerbesarKedua(0));
-console.log(getAngkaTerbesarKedua());
+// console.log(getAngkaTerbesarKedua(0));
+// console.log(getAngkaTerbesarKedua());
